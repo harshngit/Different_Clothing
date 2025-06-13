92973c7253eb267db57df038b59e0bec3be752c4
Harsh Nikharge <nikhargeharsh@gmail.com>
2025-06-13 20:03:33 +0530

    DOne

--- src/app/firebase.config.js
+++ src/app/firebase.config.js
@@ -1,6 +1,5 @@
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
@@ -23,4 +22,3 @@ export const db = getFirestore(app);
 export const database = getDatabase(app)
 export const storage = getStorage(app)
 export const auth = getAuth()
-const analytics = getAnalytics(app);
