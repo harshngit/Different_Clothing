a78b45a128e9fc724a094bd8b824fee9a7974d66
Harsh Nikharge <nikhargeharsh@gmail.com>
2025-08-04 14:53:53 +0530

    Done

--- src/app/firebase.config.js
+++ src/app/firebase.config.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,
   authDomain: "diffclothing-56379.firebaseapp.com",
   projectId: "diffclothing-56379",
-  storageBucket: "diffclothing-56379.firebasestorage.app",
+  storageBucket: "diffclothing-56379.appspot.com",
   messagingSenderId: "519827435001",
   appId: "1:519827435001:web:52fe8855febfee419ca128",
   measurementId: "G-MFCSG70YD3"
